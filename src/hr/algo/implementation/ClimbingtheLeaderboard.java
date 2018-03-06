730999f471504c7de4893ba4ec3d74d480637116
Raghavendra S <raghav.itm30@hotmail.com>
2018-03-06 17:23:47 +0530

    Commit for HR Solutions
    Commit for HR Solutions

---
+++ src/hr/algo/implementation/ClimbingtheLeaderboard.java
@@ -0,0 +1,7 @@
+package hr.algo.implementation;
+
+public class ClimbingtheLeaderboard {
+	public static void main(String args[]) {
+		System.out.println("Hello world");
+	}
+}
---
+++ src/hr/algo/implementation/FormingAMagicSquare.java
@@ -0,0 +1,7 @@
+package hr.algo.implementation;
+
+public class FormingAMagicSquare {
+	public static void main(String args[]) {
+		System.out.println("Hello world");
+	}
+}
